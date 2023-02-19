5588c6ea943722944dadb3a18ea76701298a5969
sadikcoban <coban.sadik@icloud.com>
2023-02-19 23:02:15 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# StormViewer-MacOS
+
+
+A basic image viewer MacOS application written in Swift to demonstrate the basics of MacOS development including;
+- Windows
+- View Controllers
+- Method Overrides
+- Delegates
+- Data Sources
+- Outlets
+- Cell Reuse
+- Auto Layout
+- NSViewController
+
---
+++ StormViewer-Macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StormViewer-Macos
+//
+//  Created by Sadık Çoban on 19.02.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
