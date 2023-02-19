13140182ff65b4e8dea525ecd771de8a6f491ee5
sadikcoban <coban.sadik@icloud.com>
2023-02-19 23:34:38 +0300

    setup splitviewcontroller

---
+++ StormViewer-Macos/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  StormViewer-Macos
+//
+//  Created by Sadık Çoban on 19.02.2023.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    @IBOutlet weak var imageView: NSImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ StormViewer-Macos/SourceViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SourceViewController.swift
+//  StormViewer-Macos
+//
+//  Created by Sadık Çoban on 19.02.2023.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    @IBOutlet weak var tableView: NSTableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+    }
+    
+    
+}
