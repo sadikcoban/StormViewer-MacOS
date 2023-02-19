9dfa6de6ff5f17edf5b3f8ad1c391c68fa8f0de3
sadikcoban <coban.sadik@icloud.com>
2023-02-19 23:46:48 +0300

    load image files into tableview

--- StormViewer-Macos/SourceViewController.swift
+++ StormViewer-Macos/SourceViewController.swift
@@ -8,15 +8,46 @@
 import Cocoa
 
 class SourceViewController: NSViewController {
-
+    
     @IBOutlet weak var tableView: NSTableView!
     
-
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
-        
+        loadImages()
     }
     
+    func loadImages()  {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        tableView.reloadData()
+    }
+    
+    
+}
+
+extension SourceViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        pictures.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        vw.textField?.stringValue = pictures[row]
+        return vw
+    }
+}
+
+extension SourceViewController: NSTableViewDelegate {
     
 }
